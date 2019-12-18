8b9804eafd7574e6f229605a9e1a216a7913d1bb
avichay nega <avichay881@gmail.com>
2019-12-18 02:08:08 +0200

    create group of queues

--- app/src/main/java/com/example/myapp/MangerActivity.java
+++ app/src/main/java/com/example/myapp/MangerActivity.java
@@ -6,15 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-//import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,68 +28,114 @@ import java.util.Map;
 
 public class MangerActivity extends AppCompatActivity {
     Button btback,btcreate,btmygroups;
+    TextView options ;
     EditText groupname,playersnum;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    ArrayList <String> doc = new ArrayList<>();
+    ListView mylist ;
+    ArrayAdapter <String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manger);
 
+
         btback = findViewById(R.id.btback);
         btcreate =findViewById(R.id.btcreategroup);
-        btmygroups = findViewById(R.id.btcreategroup);
-        groupname = findViewById(R.id.Textgroupnum);
+        btmygroups = findViewById(R.id.btmygroups);
+        groupname = findViewById(R.id.Textgroupname);
         playersnum = findViewById(R.id.textnumplayer);
+        mylist = findViewById(R.id.mylist);
 
-//        btcreate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String queuename= groupname.getText().toString();
-//                int num= Integer.parseInt(playersnum.getText().toString());
-//                if(queuename.isEmpty()){
-//                    groupname.setError("Please enter queuename");
-//                    groupname.requestFocus();
-//                }
-//                if(playersnum.getText().toString().isEmpty()){
-//                    playersnum.setError("Please enter number");
-//                    playersnum.requestFocus();
-//                }
-//                else if(playersnum.getText().toString().isEmpty()&&queuename.isEmpty()){
-//                    Toast.makeText(MangerActivity.this,"Fields are empty",Toast.LENGTH_LONG).show();
-//
-//                }
-//                if (!queuename.isEmpty()&&!playersnum.getText().toString().isEmpty()){
-//
-//                }
-//
-//                // Create a new user with a first and last queuename
-//                Map<String, Object> queue = new HashMap<>();
-//                ArrayList <String> players = new ArrayList<String>();
-//                queue.put("arraylist", players);
-//                queue.put("criteria", 1815);
-//                queue.put("queuename", queuename);
-//                queue.put("maxplayers", num);
-//
-//
-//                // Add a new document with a generated ID
-//                db.collection("queues")
-//                        .add(queue)
-//                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                            @Override
-//                            public void onSuccess(DocumentReference documentReference) {
-//                                Log.d("MangerActivity", "DocumentSnapshot added with ID: " + documentReference.getId());
-//
-//                            }
-//                        })
-//                        .addOnFailureListener(new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
-//                                Log.w("MangerActivity", "Error adding document", e);
-//                            }
-//                        });
-//
-//            }
-//        });
+
+
+
+
+        btcreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String queuename= groupname.getText().toString();
+                int num= Integer.parseInt(playersnum.getText().toString());
+                if(queuename.isEmpty()){
+                    groupname.setError("Please enter queue name");
+                    groupname.requestFocus();
+                }
+                if(playersnum.getText().toString().isEmpty()){
+                    playersnum.setError("Please enter number");
+                    playersnum.requestFocus();
+                }
+                else if(playersnum.getText().toString().isEmpty()&&queuename.isEmpty()){
+                    Toast.makeText(MangerActivity.this,"Fields are empty",Toast.LENGTH_LONG).show();
+
+                }
+                if (!queuename.isEmpty()&&!playersnum.getText().toString().isEmpty()){
+
+
+                    // Create a new user with a first and last queuename
+                    Map<String, Object> data = new HashMap<>();
+                    ArrayList <String> players = new ArrayList<String>();
+                    data.put("arraylist", players);
+                    data.put("criteria", 1815);
+                    data.put("queuename", queuename);
+                    data.put("maxplayers", num);
+
+
+
+                    // Add a new document with a generated ID
+                    db.collection("queues")
+                            .add(data)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d("MangerActivity", "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("MangerActivity", "Error adding document", e);
+                                }
+                            });
+                }
+
+
+
+            }
+        });
+
+        btmygroups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               doc.clear();
+               mylist.setAdapter(null);
+                db.collection("queues")
+
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                        doc.add(document.getData().get("queuename").toString());
+
+                                        Log.d("avi", document.getId() + " => " + document.getData().get("queuename").toString());
+                                    }
+                                } else {
+                                    Log.d("avi", "Error getting documents: ", task.getException());
+                                }
+                                adapter = new ArrayAdapter<String>(MangerActivity.this,android.R.layout.simple_list_item_1,doc);
+                                mylist.setAdapter(adapter);
+                            }
+                        });
+
+
+            }
+
+        });
     }
 }
+
